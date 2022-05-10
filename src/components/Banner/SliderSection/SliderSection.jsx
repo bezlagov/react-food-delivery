6dc2c7102f9e07a53ceed79829ec8ce4e287221b
Ivan <oppositelockgames@users.noreply.github.com>
2022-05-10 15:14:50 +0300

    change classname to classdata

--- src/components/Banner/SliderSection/SliderSection.jsx
+++ src/components/Banner/SliderSection/SliderSection.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const SliderSection = (props) => {
   return (
-    <section className={`swiper-slide promo ${props.className}`}>
+    <section className={`swiper-slide promo ${props.classData}`}>
       <h1 className='promo-title'>{props.title}</h1>
       <p className='promo-text'>{props.description}</p>
     </section>
