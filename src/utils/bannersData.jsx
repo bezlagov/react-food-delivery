0ce2ca669f315d58019026e5dd84f002fc2cae91
Ivan <oppositelockgames@users.noreply.github.com>
2022-05-10 15:59:21 +0300

    handle with br for banner

---
+++ src/utils/bannersData.jsx
@@ -0,0 +1,10 @@
+import { Fragment } from "react";
+
+const banners = [
+  { classData: "pizza", title: <Fragment>Online-service <br />food delivery</Fragment>, description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat exercitationem quas unde placeat! Labore quist" },
+  { classData: "kebab", title: <Fragment>Kebab on May<br />price off 35%</Fragment>, description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos reiciendis, voluptatum facere veniam officia ducimus! Soluta magnam doloremque" },
+  { classData: "vegetables", title: <Fragment>"Price off 20% for all positions<br />Use promo code LOVE.JS"</Fragment>, description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eaque, adipisci iste minus, ea veniam totam ad" },
+  { classData: "sushi", title: <Fragment>"All susie sets<br />with discount 30%"</Fragment>, description: "Lorem ipsum dolor sit amet Ab vel, voluptas ipsam laborum incidunt, re" },
+];
+
+export default banners;
